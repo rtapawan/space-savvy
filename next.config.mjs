bdd71adead13119282c006bd9d8e920ef0ad7ebf
rtapawan <rtapawan@deloitte.com>
2024-05-21 22:40:49 +0800

    test commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/space-savvy",
+  basePath: "/space-savvy/src/app",
   output: "export",
   reactStrictMode: false,
   images: {
